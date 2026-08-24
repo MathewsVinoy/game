ee364114e89456eb0b0b7d98e4716e96e14374f6
Mathews_Vinoy <mathewsvinoy@gmail.com>
2026-08-25 00:51:58 +0530

    model header

---
+++ engine/include/engine/render/model_buffers.hpp
@@ -0,0 +1,38 @@
+#pragma once
+
+#include "engine/render/devices.hpp"
+
+#define GLM_FORCE_RADIANS
+#define GLM_FORCE_DEPTH_ZERO_TO_ONE
+#include <glm/glm.hpp>
+
+#include <vector>
+
+namespace engine {
+class ModelBuffer {
+ public:
+  struct Vertex {
+    glm::vec2 position;
+
+    static std::vector<VkVertexInputBindingDescription> getBindingDescriptions();
+    static std::vector<VkVertexInputAttributeDescription> getAttributeDescriptions();
+  };
+
+  ModelBuffer(EngineDevice &device, const std::vector<Vertex> &vertices);
+  ~ModelBuffer();
+
+  ModelBuffer(const ModelBuffer &) = delete;
+  ModelBuffer &operator=(const ModelBuffer &) = delete;
+
+  void bind(VkCommandBuffer commandBuffer);
+  void draw(VkCommandBuffer commandBuffer);
+
+ private:
+  void createVertexBuffers(const std::vector<Vertex> &vertices);
+
+  EngineDevice &engineDevice;
+  VkBuffer vertexBuffer;
+  VkDeviceMemory vertexBufferMemory;
+  uint32_t vertexCount;
+};
+}  // namespace engine
