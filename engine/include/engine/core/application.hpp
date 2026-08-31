f53b97fb01ade843821ea1f199c63d2829bb9b1b
Mathews_Vinoy <mathewsvinoy@gmail.com>
2026-09-01 00:04:26 +0530

    clean up

--- engine/include/engine/core/application.hpp
+++ engine/include/engine/core/application.hpp
@@ -4,6 +4,7 @@
 #include "engine/render/pipeline.hpp"
 #include "engine/render/devices.hpp"
 #include "engine/render/swap_chain.hpp"
+#include "engine/render/object.hpp"
 
 #include <memory>
 #include <vector>
@@ -25,16 +26,22 @@ namespace engine
         void run();
 
     private:
+        void loadGameObjects();
         void createPipelineLayout();
         void createPipeline();
         void createCommandBuffers();
+        void freeCommandBuffers();
         void drawFrame();
+        void recreateSwapChain();
+        void recordCommandBuffer(int imageIndex);
+        void renderGameObjects(VkCommandBuffer commandBuffer);
 
         Window window{WIDTH, HEIGHT, "Engine"};
         EngineDevice engineDevice{window};
-        SwapChain swapChain{engineDevice, window.getExtent()};
+        std::unique_ptr<SwapChain> swapChain;
         std::unique_ptr<Pipeline> pipeline;
         VkPipelineLayout pipelineLayout;
         std::vector<VkCommandBuffer> commandBuffers;
+        std::vector<GameObject> gameObjects;
     };
 }
