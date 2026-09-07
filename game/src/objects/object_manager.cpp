2a85479c93e373baed63935c3de28fd7a03ab8ac
Mathews_Vinoy <mathewsvinoy@gmail.com>
2026-09-08 00:12:01 +0530

    make the struct proper

---
+++ game/src/objects/object_manager.cpp
@@ -0,0 +1,23 @@
+#include "game/objects/object_manager.hpp"
+
+namespace opengame
+{
+    ObjectManager::ObjectManager() {}
+
+    ObjectManager::~ObjectManager() {}
+
+    void ObjectManager::initialize()
+    {
+        // Initialize object manager resources here
+    }
+
+    void ObjectManager::update(float deltaTime)
+    {
+        // Update object manager logic here
+    }
+
+    void ObjectManager::shutdown()
+    {
+        // Clean up object manager resources here
+    }
+}
