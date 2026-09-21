7988a2a09cca3138a8d64d198ec200c9d70766c8
Mathews_Vinoy <mathewsvinoy@gmail.com>
2026-09-21 19:46:25 +0530

    added load of the fbx and animation to the game

--- engine/src/system/render_system.cpp
+++ engine/src/system/render_system.cpp
@@ -11,91 +11,99 @@
 
 namespace engine
 {
-    struct SimplePushConstantData
-    {
-        glm::mat4 modelMatrix{1.f};
-        glm::mat4 normalMatrix{1.f};
-    };
+  struct SimplePushConstantData
+  {
+    glm::mat4 modelMatrix{1.f};
+    glm::mat4 normalMatrix{1.f};
+    int uSkinned{0};
+  };
 
-    RenderSystem::RenderSystem(EngineDevice &engineDevice, VkRenderPass renderPass, VkDescriptorSetLayout globalSetLayout)
-        : engineDevice{engineDevice}
+  RenderSystem::RenderSystem(EngineDevice &engineDevice, VkRenderPass renderPass,
+                             VkDescriptorSetLayout globalSetLayout)
+      : engineDevice{engineDevice}
+  {
+    createPipelineLayout(globalSetLayout);
+    createPipeline(renderPass);
+  }
+
+  RenderSystem::~RenderSystem()
+  {
+    vkDestroyPipelineLayout(engineDevice.device(), pipelineLayout, nullptr);
+  }
+
+  void RenderSystem::createPipelineLayout(VkDescriptorSetLayout globalSetLayout)
+  {
+    VkPushConstantRange pushConstantRange{};
+    pushConstantRange.stageFlags =
+        VK_SHADER_STAGE_VERTEX_BIT | VK_SHADER_STAGE_FRAGMENT_BIT;
+    pushConstantRange.offset = 0;
+    pushConstantRange.size = sizeof(SimplePushConstantData);
+
+    std::vector<VkDescriptorSetLayout> descriptorSetLayouts{globalSetLayout};
+
+    VkPipelineLayoutCreateInfo pipelineLayoutInfo{};
+    pipelineLayoutInfo.sType = VK_STRUCTURE_TYPE_PIPELINE_LAYOUT_CREATE_INFO;
+    pipelineLayoutInfo.setLayoutCount =
+        static_cast<uint32_t>(descriptorSetLayouts.size());
+    pipelineLayoutInfo.pSetLayouts = descriptorSetLayouts.data();
+    pipelineLayoutInfo.pushConstantRangeCount = 1;
+    pipelineLayoutInfo.pPushConstantRanges = &pushConstantRange;
+    if (vkCreatePipelineLayout(engineDevice.device(), &pipelineLayoutInfo,
+                               nullptr, &pipelineLayout) != VK_SUCCESS)
     {
-        createPipelineLayout(globalSetLayout);
-        createPipeline(renderPass);
+      throw std::runtime_error("failed to create pipeline layout!");
     }
+  }
 
-    RenderSystem::~RenderSystem() { vkDestroyPipelineLayout(engineDevice.device(), pipelineLayout, nullptr); }
+  void RenderSystem::createPipeline(VkRenderPass renderPass)
+  {
+    assert(pipelineLayout != nullptr &&
+           "Cannot create pipeline before pipeline layout");
 
-    void RenderSystem::createPipelineLayout(VkDescriptorSetLayout globalSetLayout)
+    PipelineConfigInfo pipelineConfig{};
+    Pipeline::defaultPipelineConfigInfo(pipelineConfig);
+    pipelineConfig.renderPass = renderPass;
+    pipelineConfig.pipelineLayout = pipelineLayout;
+    pipeline = std::make_unique<Pipeline>(
+        engineDevice, "engine/shaders/triangle.vert.spv",
+        "engine/shaders/triangle.frag.spv", pipelineConfig);
+  }
+
+  void RenderSystem::renderGameObjects(FrameInfo &frameInfo)
+  {
+    pipeline->bind(frameInfo.commandBuffer);
+
+    vkCmdBindDescriptorSets(frameInfo.commandBuffer,
+                            VK_PIPELINE_BIND_POINT_GRAPHICS, pipelineLayout, 0, 1,
+                            &frameInfo.globalDescriptorSet, 0, nullptr);
+
+    for (auto &kv : frameInfo.gameObjects)
     {
-        VkPushConstantRange pushConstantRange{};
-        pushConstantRange.stageFlags = VK_SHADER_STAGE_VERTEX_BIT | VK_SHADER_STAGE_FRAGMENT_BIT;
-        pushConstantRange.offset = 0;
-        pushConstantRange.size = sizeof(SimplePushConstantData);
+      auto &obj = kv.second;
+      if (obj.modelBuffer == nullptr && obj.animatedModel == nullptr)
+        continue;
 
-        std::vector<VkDescriptorSetLayout> descriptorSetLayouts{globalSetLayout};
+      SimplePushConstantData push{};
+      push.modelMatrix = obj.transform.mat4();
+      push.normalMatrix = glm::mat4(
+          glm::transpose(glm::inverse(glm::mat3(obj.transform.mat4()))));
+      push.uSkinned = (obj.animatedModel != nullptr) ? 1 : 0;
 
-        VkPipelineLayoutCreateInfo pipelineLayoutInfo{};
-        pipelineLayoutInfo.sType = VK_STRUCTURE_TYPE_PIPELINE_LAYOUT_CREATE_INFO;
-        pipelineLayoutInfo.setLayoutCount = static_cast<uint32_t>(descriptorSetLayouts.size());
-        pipelineLayoutInfo.pSetLayouts = descriptorSetLayouts.data();
-        pipelineLayoutInfo.pushConstantRangeCount = 1;
-        pipelineLayoutInfo.pPushConstantRanges = &pushConstantRange;
-        if (vkCreatePipelineLayout(engineDevice.device(), &pipelineLayoutInfo, nullptr, &pipelineLayout) !=
-            VK_SUCCESS)
-        {
-            throw std::runtime_error("failed to create pipeline layout!");
-        }
+      vkCmdPushConstants(frameInfo.commandBuffer, pipelineLayout,
+                         VK_SHADER_STAGE_VERTEX_BIT |
+                             VK_SHADER_STAGE_FRAGMENT_BIT,
+                         0, sizeof(SimplePushConstantData), &push);
+      if (obj.animatedModel)
+      {
+        obj.animatedModel->getModel()->bind(frameInfo.commandBuffer);
+        obj.animatedModel->getModel()->draw(frameInfo.commandBuffer);
+      }
+      else
+      {
+        obj.modelBuffer->bind(frameInfo.commandBuffer);
+        obj.modelBuffer->draw(frameInfo.commandBuffer);
+      }
     }
-
-    void RenderSystem::createPipeline(VkRenderPass renderPass)
-    {
-        assert(pipelineLayout != nullptr && "Cannot create pipeline before pipeline layout");
-
-        PipelineConfigInfo pipelineConfig{};
-        Pipeline::defaultPipelineConfigInfo(pipelineConfig);
-        pipelineConfig.renderPass = renderPass;
-        pipelineConfig.pipelineLayout = pipelineLayout;
-        pipeline = std::make_unique<Pipeline>(
-            engineDevice,
-            "engine/shaders/triangle.vert.spv",
-            "engine/shaders/triangle.frag.spv",
-            pipelineConfig);
-    }
-
-    void RenderSystem::renderGameObjects(FrameInfo &frameInfo)
-    {
-        pipeline->bind(frameInfo.commandBuffer);
-
-        vkCmdBindDescriptorSets(
-            frameInfo.commandBuffer,
-            VK_PIPELINE_BIND_POINT_GRAPHICS,
-            pipelineLayout,
-            0,
-            1,
-            &frameInfo.globalDescriptorSet,
-            0,
-            nullptr);
-
-        for (auto &kv : frameInfo.gameObjects)
-        {
-            auto &obj = kv.second;
-            if (obj.modelBuffer == nullptr)
-                continue;
-            SimplePushConstantData push{};
-            push.modelMatrix = obj.transform.mat4();
-            push.normalMatrix = glm::mat4(glm::transpose(glm::inverse(glm::mat3(obj.transform.mat4()))));
-
-            vkCmdPushConstants(
-                frameInfo.commandBuffer,
-                pipelineLayout,
-                VK_SHADER_STAGE_VERTEX_BIT | VK_SHADER_STAGE_FRAGMENT_BIT,
-                0,
-                sizeof(SimplePushConstantData),
-                &push);
-            obj.modelBuffer->bind(frameInfo.commandBuffer);
-            obj.modelBuffer->draw(frameInfo.commandBuffer);
-        }
-    }
+  }
 
 } // namespace engine
