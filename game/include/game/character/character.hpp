1263ce38532adbc6e0e1783cb1c8a89660cba53a
Mathews_Vinoy <mathewsvinoy@gmail.com>
2026-09-21 19:46:28 +0530

    added load of the fbx and animation to the game

--- game/include/game/character/character.hpp
+++ game/include/game/character/character.hpp
@@ -1,38 +1,38 @@
 #pragma once
 
-#include "game/objects/objects.hpp"
 #include "engine/input/keyboard_controller.hpp"
+#include "game/objects/objects.hpp"
 
 namespace engine
 {
-    class Application;
+  class Application;
 }
 
 namespace opengame
 {
-    class Character
-    {
-    public:
-        Character();
-        ~Character();
+  class Character
+  {
+  public:
+    Character();
+    ~Character();
 
-        Object getObject();
-        void move(float dt);
-        void loadCharacterModel(engine::Application &app);
-        void resolveGroundCollision();
+    Object getObject();
+    void move(float dt);
+    void loadCharacterModel(engine::Application &app);
+    void resolveGroundCollision();
 
-    private:
-        Object object;
-        engine::KeyboardMovementController controller;
-        engine::KeyboardMovementController::KeyMappings keyMappings;
-        engine::Application *application{nullptr};
-        engine::GameObject::id_t renderObjectId{0};
+  private:
+    Object object;
+    engine::KeyboardMovementController controller;
+    engine::KeyboardMovementController::KeyMappings keyMappings;
+    engine::Application *application{nullptr};
+    engine::GameObject::id_t renderObjectId{0};
 
-        float moveSpeed = 5.0f;
-        float verticalVelocity = 0.0f;
-        bool isGrounded = true;
-        const float GRAVITY_STRENGTH = 14.0f;
-        const float JUMP_STRENGTH = -6.0f;
-        const float GROUND_LEVEL = 0.0f;
-    };
-}
+    float moveSpeed = 5.0f;
+    float verticalVelocity = 0.0f;
+    bool isGrounded = true;
+    const float GRAVITY_STRENGTH = 14.0f;
+    const float JUMP_STRENGTH = -6.0f;
+    const float GROUND_LEVEL = 0.0f;
+  };
+} // namespace opengame
