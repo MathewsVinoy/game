507c8305e288c7eff1419944d70623b65f60d241
Mathews_Vinoy <mathewsvinoy@gmail.com>
2026-09-21 19:44:22 +0530

    added load of the fbx and animation to the game

--- engine/include/engine/core/application.hpp
+++ engine/include/engine/core/application.hpp
@@ -1,54 +1,57 @@
 #pragma once
 
+#include "engine/core/descriptors.hpp"
 #include "engine/core/window.hpp"
 #include "engine/render/devices.hpp"
 #include "engine/render/object.hpp"
 #include "engine/render/renderer.hpp"
-#include "engine/core/descriptors.hpp"
+#include "engine/animation/animated_model.hpp"
 
 #include <functional>
 #include <memory>
-#include <vector>
 #include <string>
+#include <vector>
 
 namespace engine
 {
-    class Application
-    {
-    public:
-        static constexpr int WIDTH = 800;
-        static constexpr int HEIGHT = 600;
+  class Application
+  {
+  public:
+    static constexpr int WIDTH = 800;
+    static constexpr int HEIGHT = 600;
 
-        Application();
-        ~Application();
+    Application();
+    ~Application();
 
-        Application(const Application &) = delete;
-        Application &operator=(const Application &) = delete;
+    Application(const Application &) = delete;
+    Application &operator=(const Application &) = delete;
 
-        using UpdateCallback = std::function<void(float)>;
+    using UpdateCallback = std::function<void(float)>;
 
-        void setUpdateCallback(UpdateCallback callback);
-        void run();
-        GameObject::id_t renderGameObjects(std::string modelPath,
-                                           glm::vec3 translation = {0.f, 0.f, 0.f},
-                                           glm::vec3 scale = {1.f, 1.f, 1.f},
-                                           glm::vec3 rotation = {0.f, 0.f, 0.f});
+    void setUpdateCallback(UpdateCallback callback);
+    void run();
+    GameObject::id_t renderGameObjects(std::string modelPath,
+                                       glm::vec3 translation = {0.f, 0.f, 0.f},
+                                       glm::vec3 scale = {1.f, 1.f, 1.f},
+                                       glm::vec3 rotation = {0.f, 0.f, 0.f});
 
-        Window &getWindow();
-        GameObject::Map &getGameObjects();
+    Window &getWindow();
+    GameObject::Map &getGameObjects();
 
-        float getYaw() const { return yaw; }
+    float getYaw() const { return yaw; }
 
-    private:
-        void loadGameObjects();
-        float yaw = 0.f;
+  private:
+    void loadGameObjects();
 
-        Window window{WIDTH, HEIGHT, "Engine"};
-        EngineDevice engineDevice{window};
-        Renderer renderer{window, engineDevice};
+    float yaw = 0.f;
 
-        std::unique_ptr<DescriptorPool> globalPool{};
-        GameObject::Map gameObjects;
-        UpdateCallback updateCallback{};
-    };
-}
+    Window window{WIDTH, HEIGHT, "Engine"};
+    EngineDevice engineDevice{window};
+    Renderer renderer{window, engineDevice};
+
+    std::unique_ptr<DescriptorPool> globalPool{};
+    GameObject::Map gameObjects;
+    UpdateCallback updateCallback{};
+    std::shared_ptr<AnimatedModel> animatedCharacter;
+  };
+} // namespace engine
