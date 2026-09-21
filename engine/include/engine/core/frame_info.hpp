717c91c46d6940b582d0039b7dd6165e2dbaec61
Mathews_Vinoy <mathewsvinoy@gmail.com>
2026-09-21 19:44:27 +0530

    added load of the fbx and animation to the game

--- engine/include/engine/core/frame_info.hpp
+++ engine/include/engine/core/frame_info.hpp
@@ -10,10 +10,10 @@ namespace engine
 
 #define MAX_LIGHTS 10
 
-    struct PointLight
+    struct Sunlight
     {
-        glm::vec4 position{}; // ignore w
-        glm::vec4 color{};    // w is intensity
+        glm::vec4 direction{0.f, 0.f, 0.f, 0.f}; // std140 alignment: vec3 + padding
+        glm::vec4 color{1.f, 1.f, 1.f, 1.f};     // RGB color + intensity (w)
     };
 
     struct GlobalUbo
@@ -22,8 +22,8 @@ namespace engine
         glm::mat4 view{1.f};
         glm::mat4 inverseView{1.f};
         glm::vec4 ambientLightColor{1.f, 1.f, 1.f, .02f}; // w is intensity
-        PointLight pointLights[MAX_LIGHTS];
-        int numLights;
+        Sunlight sunlight{};
+        int numLights{0};
     };
 
     struct BoneUbo
