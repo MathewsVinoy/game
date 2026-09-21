db39576748b07c54cbeff1156c2502bd8a77bb56
Mathews_Vinoy <mathewsvinoy@gmail.com>
2026-09-21 19:45:03 +0530

    added load of the fbx and animation to the game

--- engine/include/engine/system/render_system.hpp
+++ engine/include/engine/system/render_system.hpp
@@ -1,34 +1,33 @@
 #pragma once
 
-#include "engine/render/pipeline.hpp"
+#include "engine/core/frame_info.hpp"
+#include "engine/render/camera.hpp"
 #include "engine/render/devices.hpp"
 #include "engine/render/object.hpp"
-#include "engine/render/camera.hpp"
-#include "engine/core/frame_info.hpp"
+#include "engine/render/pipeline.hpp"
 
 #include <memory>
 #include <vector>
 
-namespace engine
-{
-    class RenderSystem
-    {
-    public:
-        RenderSystem(EngineDevice &engineDevice, VkRenderPass renderPass, VkDescriptorSetLayout globalSetLayout);
-        ~RenderSystem();
+namespace engine {
+class RenderSystem {
+public:
+  RenderSystem(EngineDevice &engineDevice, VkRenderPass renderPass,
+               VkDescriptorSetLayout globalSetLayout);
+  ~RenderSystem();
 
-        RenderSystem(const RenderSystem &) = delete;
-        RenderSystem &operator=(const RenderSystem &) = delete;
+  RenderSystem(const RenderSystem &) = delete;
+  RenderSystem &operator=(const RenderSystem &) = delete;
 
-        void renderGameObjects(FrameInfo &frameInfo);
+  void renderGameObjects(FrameInfo &frameInfo);
 
-    private:
-        void createPipelineLayout(VkDescriptorSetLayout globalSetLayout);
-        void createPipeline(VkRenderPass renderPass);
+private:
+  void createPipelineLayout(VkDescriptorSetLayout globalSetLayout);
+  void createPipeline(VkRenderPass renderPass);
 
-        EngineDevice &engineDevice;
+  EngineDevice &engineDevice;
 
-        std::unique_ptr<Pipeline> pipeline;
-        VkPipelineLayout pipelineLayout;
-    };
-}
+  std::unique_ptr<Pipeline> pipeline;
+  VkPipelineLayout pipelineLayout;
+};
+} // namespace engine
