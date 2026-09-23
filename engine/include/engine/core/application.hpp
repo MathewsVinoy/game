50a4ae7d445ae432c4790aadf7aecbfda69b0de6
Mathews_Vinoy <mathewsvinoy@gmail.com>
2026-09-23 18:57:01 +0530

    restart with basic app code

---
+++ engine/include/engine/core/application.hpp
@@ -0,0 +1,19 @@
+#pragma once
+
+namespace engine
+{
+    class Application
+    {
+    public:
+        static constexpr int WIDTH = 800;
+        static constexpr int HEIGHT = 600;
+
+        Application();
+        ~Application();
+
+        Application(const Application &) = delete;
+        Application &operator=(const Application &) = delete;
+
+    private:
+        };
+}
